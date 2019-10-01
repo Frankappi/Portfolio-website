6e10512d870eeb1be9679e31ffad3d63be7718b7
Frankappi <frankerd93@live.nl>
2019-10-01 16:50:55 +0200

    Update index.php

--- beheer/index.php
+++ beheer/index.php
@@ -3,30 +3,18 @@
 ?>
 
 <section class="content">
-  <div class="col-md-12">
-    <div class="container">
-      <div class="col-md-3"></div>
-      <div class="col-md-6">
-        <div class="col-md-12 text-center logbox">
-          <div class="col-md-2"></div>
-          <div class="col-md-8">
-            <form class="logform" action="dashboard.php">
-              <div class="form-group">
-                <input type="text" class="form-control loginput" placeholder="Naam">
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control loginput" placeholder="Wachtwoord">
-              </div>
-              <div class="form-group">
-                <input type="submit" class="btn logbtn" role="button" value="Login">
-              </div>
-            </form>
-          </div>
-          <div class="col-md-2"></div>
-        </div>
+  <div class="col-sm-6 col-12 mx-auto">
+    <form class="logform" action="dashboard.php">
+      <div class="form-group">
+       <input type="text" class="form-control loginput" placeholder="Naam">
       </div>
-      <div class="col-md-3"></div>
-    </div>
+      <div class="form-group">
+       <input type="password" class="form-control loginput" placeholder="Wachtwoord">
+      </div>
+      <div class="form-group">
+        <input type="submit" class="btn logbtn" role="button" value="Login">
+      </div>
+    </form>
   </div>
 </section>
 
